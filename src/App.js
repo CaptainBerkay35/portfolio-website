924d2d4c9f78d806f42ee21048d6743e8f8bfb8c
Berkay Kaptan <kaptanberkay35@hotmail.com>
2024-10-21 17:05:14 +0300

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import './App.css';
+import Aos from "aos";
+
+import "./App.css";
+import { useEffect } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/UI/Hero";
+import Services from "./components/UI/Services";
+import Portfolio from "./components/UI/Portfolio";
+import Contact from "./components/UI/Contact";
 
 function App() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return (
-    <h1>Hello</h1>
+    <>
+      <Header />
+
+      <main>
+        <Hero />
+        <Services />
+        <Portfolio/>
+        <Contact />
+      </main>
+      <Footer />
+    </>
   );
 }
 
